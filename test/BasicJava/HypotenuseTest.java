b0731519aa9d4af45c7ce4c5e859284a4543658e
KaiyangYao <kyao@macalester.edu>
2019-09-10 00:59:04 -0500

    comp127-f19/hw0

---
+++ test/BasicJava/HypotenuseTest.java
@@ -0,0 +1,13 @@
+package BasicJava;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HypotenuseTest {
+    @Test
+    public void testSimple(){
+        assertEquals(5,Hypotenuse.calculateHypotenuse(3,4));
+    }
+
+}
